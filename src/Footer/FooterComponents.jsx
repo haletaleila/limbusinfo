c69bfe6a64d7dc3baa2c3cad913f786f302e4033
haletaleila <leilahaleta@gmail.com>
2023-09-04 01:38:55 +0900

    업데이트 내용

--- src/Footer/FooterComponents.jsx
+++ src/Footer/FooterComponents.jsx
@@ -8,7 +8,7 @@ function FooterComponents() {
       <FooterContainer>
         <p>Copyright &copy; {currentYear} 하레타 All rights reserved.</p>
         <p style={{ fontSize: "14px", color: "#888", marginLeft: "10px" }}>
-          version 1.0.0
+          version 1.0.1
         </p>
       </FooterContainer>
     </>
