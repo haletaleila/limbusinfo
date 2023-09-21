72630caa6c9e2e6e4d4076bddb714527b8964fbf
haletaleila <leilahaleta@gmail.com>
2023-09-21 19:47:03 +0900

    230921 gg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./Router"; // Import AppRouter
 
 const root = document.getElementById("root");
